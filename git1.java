caff7750376216fc0157d10cb64cd7e600885461
ludya3011 <57378877+ludya3011@users.noreply.github.com>
2019-11-05 13:59:41 +0700

    Update git1.java

--- git1.java
+++ git1.java
@@ -3,14 +3,8 @@ import java.io.*;
 
 public class git1 {
 	public static void main(String[] args) {
-		System.out.println("This is version five");
-		System.out.println("Hello World - this is new version");
-		
-		System.out.println("This is version SIX");
-		System.out.println("Hello World 6 - this is new version");
-		
-		System.out.println("This is version Seven");
-		System.out.println("Hello World 7 - this is new version");
+		System.out.println("This is version ultimate");
+		System.out.println("Hello World - this is new version")
 
 	}
 }
