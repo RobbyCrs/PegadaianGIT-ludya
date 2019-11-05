bd051d466d6ebdd274594a32eaf5f23cc1c5791c
ludya3011 <57378877+ludya3011@users.noreply.github.com>
2019-11-05 09:57:29 +0700

    Update git1.java

--- git1.java
+++ git1.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class git1 {
 	public static void main(String[] args) {
-		System.out.println("This is version one");
+		System.out.println("This is version two");
 		System.out.println("Hello World");
 	}
 }
