09af31fc3af81170beca3906c3d59400324618ad
palupik <palupisekarh@gmail.com>
2019-11-05 13:39:01 +0700

    Update git1.java

--- git1.java
+++ git1.java
@@ -5,5 +5,9 @@ public class git1 {
 	public static void main(String[] args) {
 		System.out.println("This is version five");
 		System.out.println("Hello World - this is new version");
+		
+		System.out.println("This is version SIX");
+		System.out.println("Hello World 6 - this is new version");
+
 	}
 }
